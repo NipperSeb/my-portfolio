2643739440ce0cefdf41d811dc238fd6c9828da8
NipperSeb <sebastien.pincepoche@wanadoo.fr>
2023-06-26 16:12:11 +0200

    first commit

---
+++ app/components/header.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import React from 'react'
+
+export const Header = () => {
+  return (
+   <header>
+    <div>
+      <nav>
+        3 links
+      </nav>
+      <h1 className="">SEBASTIEN PINCEPOCHE</h1>
+    </div>
+    <Image priority height={400} width={2000} className='' src='' alt='' aria-label=""/>
+   </header>
+  )
+}
---
+++ app/data/data.tsx
@@ -0,0 +1,42 @@
+export const pages = [
+  {
+  id:1,
+  label:"À propos de moi",
+  link:"/",
+  },
+  {
+  id:2,
+  label: "Mes projets",
+  link: "/my-projects",
+  },
+  {
+  id:3,
+  label: "Me contacter",
+  link : "/contact-me"
+  },
+]
+
+export const aboutMeData = {
+  title:"Bonjour! Je suis Sébastien Pincepoche",
+  body: "Riche d'une expérience de plus de quinze ans dans la vente en tant que Chef de Rayon, je m'intéresse au Digital et ua numérique depuis 5 ans.",
+  highLightedBody: "Actuellement, je réalise divers projets afin d'obtenir mon RNCP 'Developpeur Intégrateur Web'",
+  body2:"Je souhaite devenir expert webmarketing et recherche une entreprise pour suivre une alternance 'Chef de projet digital' pour la rentrée prochaine avec l'école 301"
+}
+
+export const contactMeText = {
+  title: "Envie de mieux me connaître",
+  body: "mon profil vous intéresse et voulez en savoir plus"
+}
+
+export const projects = [
+  {
+    title: "",
+    body1:"",
+    body2:"",
+    image:"",
+    links :[
+      {text:"Source Code ",
+    url:""}
+    ]
+  }
+]
